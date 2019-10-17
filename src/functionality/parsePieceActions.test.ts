e41e428e70347536c39b7533e970e4cac8607a1b
FTWinston <ftwinston@gmail.com>
2019-10-17 23:26:48 +0100

    Now parsing the simplest piece actions. Implemented tests.

--- src/functionality/parsePieceActions.test.ts
+++ src/functionality/parsePieceActions.test.ts
@@ -1,19 +1,61 @@
 import { parsePieceActions } from './parsePieceActions';
+import { MoveType } from './MoveType';
 
-it('parses simple example action', () => {
-    const result = parsePieceActions('It can slide 1 cell forward.');
-    
-    if (result.success) {
-        expect(result.definition).toHaveLength(1);
+type SimpleExample = [string, MoveType, string, number, number | undefined];
 
-        const definition = result.definition[0];
-        expect(definition.minDistance).toBe(1);
-        expect(definition.maxDistance).toBe(1);
-        expect(definition.direction).toBe('forward');
+it('parses simple example actions', () => {
+    const examples: SimpleExample[] = [
+        ['It can slide 1 cell forward.', MoveType.Slide, 'forward', 1, 1],
+        ['It can slide any distance diagonally.', MoveType.Slide, 'diagonally', 1, undefined],
+        ['It can hop up to 3 cells orthogonally.', MoveType.Hop, 'orthogonally', 1, 3],
+        ['It can leap 2 to 4 cells orthogonally.', MoveType.Leap, 'orthogonally', 2, 4],
+        ['It can leap 2 cells diagonally.', MoveType.Leap, 'diagonally', 2, 2],
+        ['It can slide at least 2 cells orthogonally.', MoveType.Slide, 'orthogonally', 2, undefined],
+    ];
+
+    for (const example of examples) {
+        const result = parsePieceActions(example[0]);
+        
+        if (result.success) {
+            expect(result.definition).toHaveLength(1);
+
+            const definition = result.definition[0];
+            expect(definition.moveType).toBe(example[1]);
+            expect(definition.direction).toBe(example[2]);
+            expect(definition.minDistance).toBe(example[3]);
+            expect(definition.maxDistance).toBe(example[4]);
+        }
+        else {
+            expect(result.errors).toHaveLength(0);
+        }
+
+        expect(result.success).toBe(true);
     }
-    else {
-        expect(result.errors).toHaveLength(0);
-    }
+});
 
-    expect(result.success).toBe(true);
+
+type SimpleErrorExample = [string, string, number, number];
+
+it('Correctly marks errors in erroneous simple examples', () => {
+    const examples: SimpleErrorExample[] = [
+        ['It can slide 0 cells forward.', 'Distance value must be greater than zero.', 13, 1],
+        ['It can leap 4 to 2 cells orthogonally.', 'Second distance value must be greater than the first value.', 17, 1],
+        ['It can leap 50 to 17 cells orthogonally.', 'Second distance value must be greater than the first value.', 18, 2],
+        ['It can hop any quantity of cells orthogonally.', 'Unrecognised distance: any quantity of cells.', 11, 21],
+    ];
+
+    for (const example of examples) {
+        const result = parsePieceActions(example[0]);
+        
+        if (!result.success) {
+            expect(result.errors).toHaveLength(1);
+
+            const error = result.errors[0];
+            expect(error.message).toContain(example[1]);
+            expect(error.startIndex).toBe(example[2]);
+            expect(error.length).toBe(example[3]);
+        }
+
+        expect(result.success).toBe(false);
+    }
 });
--- src/functionality/parsePieceActions.ts
+++ src/functionality/parsePieceActions.ts
@@ -1,72 +1,132 @@
 import { ConfigurationParser, IParserError } from 'natural-configuration';
 import { PieceActionDefinition } from './PieceActionDefinition';
 import { MoveType } from './MoveType';
+import { CellSelector } from '../editor/board/pages/CellSelector';
 
 const parser = new ConfigurationParser<PieceActionDefinition[]>([
     {
         type: 'standard',
-        expressionText: 'It can (\\w+) (any distance|(at least |at most |up to |\\d+ to )?(\\d+) cells?) (.+)',
+        expressionText: 'It can (?<moveType>\\w+) (?<distance>any distance|.+ cells?) (?<direction>.+)',
         parseMatch: (match, action, error) => {
             let success = true;
+            const groups = match.groups!;
             
-            const strMoveType = match[1].toLowerCase();
-            let moveType: MoveType;
-
-            switch (strMoveType) {
-                case 'slide':
-                    moveType = MoveType.Slide;
-                    break;
-                case 'leap':
-                    moveType = MoveType.Leap;
-                    break;
-                case 'hop':
-                    moveType = MoveType.Hop;
-                    break;
-                default:
-                    error({
-                        startIndex: 7,
-                        length: strMoveType.length,
-                        message: `Unrecognised move type: ${strMoveType}. Allowed values are: slide, leap or hop.`,
-                    });
-                    success = false;
-                    break;
+            let groupStartPos = 7;
+            const strMoveType = groups['moveType'];
+            const moveType = parseMoveType(strMoveType, groupStartPos, error);
+            if (moveType === undefined) {
+                success = false;
             }
 
-            /*
-            if (match[1].length === 0) {
-                error({
-                    startIndex: 8,
-                    length: 2,
-                    message: 'Match text cannot be empty.'
-                });
-        
-                return;
+            groupStartPos += strMoveType.length + 1;
+            const [minDistance, maxDistance] = parseDistance(groups['distance'], groupStartPos, error);
+            if (minDistance === undefined) {
+                success = false;
             }
-        
-            const before = new RegExp(match[1], 'g');
-            const after = match[2];
-        
-            action(modify => modify.value = modify.value.replace(before, after));
-            */
 
-            const minDistance = 1;
-            const maxDistance = undefined;
-            const direction = 'forward';
+            const direction = groups['direction'];
+            
+            // console.log('match', match);
 
             if (success) {
-                action(modify => modify.push(new PieceActionDefinition(moveType, direction, minDistance, maxDistance)));
+                action(modify => modify.push(new PieceActionDefinition(moveType!, direction, minDistance!, maxDistance)));
             }
         },
         examples: [
+            'It can slide 1 cell forward',
             'It can slide any distance diagonally',
             'It can hop up to 3 cells orthogonally',
             'It can leap 2 to 4 cells orthogonally',
-            'It can slide 1 cell forward',
             'It can leap 2 cells diagonally',
             'It can slide at least 2 cells orthogonally',
         ]
     },
 ]);
+
+function parseMoveType(moveType: string, startIndex: number, error: (error: IParserError) => void) {
+    switch (moveType.toLowerCase()) {
+        case 'slide':
+            return MoveType.Slide;
+        case 'leap':
+            return MoveType.Leap;
+        case 'hop':
+            return MoveType.Hop;
+    }
+
+    error({
+        startIndex,
+        length: moveType.length,
+        message: `Unrecognised move type: ${moveType}. Allowed values are: slide, leap or hop.`,
+    });
+}
+
+const distanceExpression = new RegExp("(?:(?<distType>at least|at most|up to|(?<firstDist>\\d+) to) )?(?<mainDist>\\d+) cells?");
+
+function parseDistance(
+    distance: string, startIndex: number, error: (error: IParserError) => void
+): [number | undefined, number | undefined] {
+    if (distance === 'any distance') {
+        return [1, undefined];
+    }
+
+    const match = distance.match(distanceExpression);
+    
+    if (match === null) {
+        error({
+            startIndex,
+            length: distance.length,
+            message: `Unrecognised distance: ${distance}. Valid distances include: "any distance", "1 cell", "up to 3 cells", "2 to 5 cells", "at least 2 cells", "at most 5 cells", "up to 4 cells".`,
+        });
+
+        return [undefined, undefined];
+    }
+
+    const mainDist = parseInt(match.groups!['mainDist']);
+    if (mainDist === 0) {
+        error({
+            startIndex: startIndex + distance.lastIndexOf(' ') - 1,
+            length: 1,
+            message: `Distance value must be greater than zero.`,
+        });
+
+        return [undefined, undefined];
+    }
+
+    switch (match.groups!['distType']) {
+        case undefined:
+            return [mainDist, mainDist];
+        case 'at least':
+            return [mainDist, undefined];
+        case 'at most':
+        case 'up to':
+            return [1, mainDist];
+    }
+    
+    const firstDist = parseInt(match.groups!['firstDist']);
+    
+    if (firstDist === 0) {
+        error({
+            startIndex,
+            length: 1,
+            message: `Distance value must be greater than zero.`,
+        });
+        return [undefined, undefined];
+    }
+
+    if (firstDist >= mainDist) {
+        const numberEnd = distance.lastIndexOf(' ');
+        const numberStart = distance.lastIndexOf(' ', numberEnd - 1) + 1;
+
+        error({
+            startIndex: startIndex + numberStart,
+            length: numberEnd - numberStart,
+            message: `Second distance value must be greater than the first value.`,
+        });
+        return [undefined, undefined];
+    }
+
+    return [firstDist, mainDist];
+}
 
 type ParseResult = {
     success: true;

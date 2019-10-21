cfa878c102cf186421fc8edcc0dd36b71a73f8b8
FTWinston <ftwinston@gmail.com>
2019-10-21 18:01:02 +0100

    Started work on movement calculation

--- src/functionality/definitions/PieceActionDefinition.ts
+++ src/functionality/definitions/PieceActionDefinition.ts
@@ -1,5 +1,8 @@
 import { MoveType } from './MoveType';
 import { PieceActionCondition } from './PieceActionCondition';
+import { GameDefinition } from './GameDefinition';
+import { IPlayerAction } from '../instances/IPlayerAction';
+import { IGameState } from '../instances/IGameState';
 
 interface IPieceActionElement {
     readonly directions: ReadonlyArray<string>;
@@ -11,5 +14,42 @@ interface IPieceActionElement {
 export class PieceActionDefinition {
     constructor(readonly moveType: MoveType, readonly moveSequence: ReadonlyArray<IPieceActionElement>, readonly conditions: PieceActionCondition[]) {
         
+    }
+
+    public getPossibleActions(game: GameDefinition, state: IGameState, board: string, cell: string, piece: number) {
+        const boardDef = game.boards.get(board);
+        if (boardDef === undefined) {
+            return [];
+        }
+
+        const boardState = state.boards[board];
+        if (boardState === undefined) {
+            return [];
+        }
+
+        const fromCellContent = boardState.cellContents[cell];
+        if (fromCellContent === undefined) {
+            return [];
+        }
+
+        const pieceData = fromCellContent.find(p => p.id === piece);
+
+        if (pieceData === undefined) {
+            return [];
+        }
+
+        const actions: IPlayerAction[] = [];
+
+        let previousDirection: string | null = null;
+
+        for (const actionElement of this.moveSequence) {
+            for (const direction of actionElement.directions) {
+                const actualDirections = boardDef.resolveDirection(direction, pieceData.owner, previousDirection);
+
+                // TODO: trace in these directions for the approriate distances
+            }
+        }
+
+        return actions;
     }
 }

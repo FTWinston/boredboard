6ee5a1c55f3ae1b76c51877dbb18202fac21e50e
FTWinston <ftwinston@gmail.com>
2019-11-14 15:49:40 +0000

    Piece behaviour editor now uses natural configuration ui component

--- src/editor/piece/pages/BehaviourEditor.tsx
+++ src/editor/piece/pages/BehaviourEditor.tsx
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import { ConfigTextArea,  } from 'natural-configuration-ui';
 import './BehaviourEditor.css';
 import { PieceDispatch } from '../PieceEditor';
+import { IParserError } from 'natural-configuration';
 
 interface Props {
     behaviour: string;
@@ -11,20 +13,34 @@ interface Props {
 
 export const BehaviourEditor: React.FunctionComponent<Props> = props => {
     const context = useContext(PieceDispatch);
+
+    const [text, setText] = useState(props.behaviour);
+
+    const errors = useMemo(() => {
+        // TODO: parse behaviour text and find any errors
+        return [] as IParserError[];
+    }, [text]);
     
     return (
         <div className="pieceEditor behaviourEditor">
             <p>Control how piece behaves</p>
 
+            <ConfigTextArea
+                className="behaviourEditor__text"
+                text={text}
+                onChange={t => setText(t)}
+                errors={errors}
+            />
+
             <div className="boardEditor__navigation">
                 <Link to={props.prevPage}>Back</Link>
                 <Link
                     to={props.nextPage}
+                    onClick={() => context({ type: 'set behaviour', behaviour: text })}
                 >
                     Continue
                 </Link>
             </div>
         </div>
     );
-    // onClick={() => { if (props.linkTypes.length === 0) { context({ type: 'set link types', linkTypes: [''] }); } }}
 }
